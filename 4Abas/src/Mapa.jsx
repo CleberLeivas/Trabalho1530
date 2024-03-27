e8a627be93b33ce3b03bd4ab84dddf387a722ad8
CleberLeivas <crebis@hotmail.com>
2024-03-27 17:05:11 -0300

    fazendo mapa

--- 4Abas/src/Mapa.jsx
+++ 4Abas/src/Mapa.jsx
@@ -2,18 +2,23 @@ import React from 'react';
 import style from './Mapa.module.css'
 import Menu from './components/Menu';
 
+import 'leaflet/dist/leaflet.css';
+import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css';
+import "leaflet-defaulticon-compatibility";
+
 import { MapContainer, TileLayer, Marker, Popup} from 'react-leaflet'
 
 export default function Mapa() {
   const position = [51.505, -0.09]
   return(
-    <div>
-      <Menu/>
-      <h1>Mapa</h1>
-      <br />
+    <div className={style.firstDiv}>
       <div>
-        <div className={style.wrapMap}>
-          <h2>Map</h2>
+        <Menu/>
+      </div>
+      <div className={style.wrapMap}>
+        <h1>Mapa</h1>
+        <br />
+        <div>
           <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
             <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
--- 4Abas/src/MarvelAPI.jsx
+++ 4Abas/src/MarvelAPI.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import style from './MarvelAPI.module.css'
+import Menu from './components/Menu';
 
 export default function MarvelAPI() {
-  return (
-    <div className={style.firstDiv}>
+  return(
+    <div>
+      <Menu/>
       <h1>API</h1>
+      <br />
     </div>
   );
 }
