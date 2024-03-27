2c6934995e5f40f4bc77663a788f01ce80755b45
CleberLeivas <crebis@hotmail.com>
2024-03-27 16:35:06 -0300

    inicialMapa

--- 4Abas/src/Calculadora.jsx
+++ 4Abas/src/Calculadora.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import style from './Calculadora.module.css'
+import Menu from './components/Menu';
 
 export default function Calculadora() {
   const [num1, setNum1] = useState('');
@@ -48,205 +49,210 @@ export default function Calculadora() {
 
   return (
     <div className={style.firstDiv}>
-      <h1>Calculadora</h1>
-      <div className="principal">
-        <div className="cards">
-          <h2>Soma</h2>
-          <div className="columnLabelWithInput">
-            <label htmlFor="num1">Numero 01</label>
-            <input
-              className="inputs-style"
-              type="number"
-              id="num1"
-              placeholder="Insira um número"
-              value={num1}
-              onChange={(e) => setNum1(e.target.value)}
-            />
-          </div>
-          <div className="columnLabelWithInput">
-            <label htmlFor="num2">Numero 02</label>
-            <input
-              className="inputs-style"
-              type="number"
-              id="num2"
-              placeholder="Insira um número"
-              value={num2}
-              onChange={(e) => setNum2(e.target.value)}
-            />
-          </div>
-          <div>
-            <button onClick={calcularSoma}>CALCULAR</button>
-          </div>
-          <div>
-            <p>Resultado: <span>{resultadoSoma}</span></p>
+      <div>
+      <Menu/>
+      </div>
+      <div className={style.secondDiv}>
+        <h1>Calculadora</h1>
+        <div className="principal">
+          <div className="cards">
+            <h2>Soma</h2>
+            <div className="columnLabelWithInput">
+              <label htmlFor="num1">Numero 01</label>
+              <input
+                className="inputs-style"
+                type="number"
+                id="num1"
+                placeholder="Insira um número"
+                value={num1}
+                onChange={(e) => setNum1(e.target.value)}
+                />
+            </div>
+            <div className="columnLabelWithInput">
+              <label htmlFor="num2">Numero 02</label>
+              <input
+                className="inputs-style"
+                type="number"
+                id="num2"
+                placeholder="Insira um número"
+                value={num2}
+                onChange={(e) => setNum2(e.target.value)}
+                />
+            </div>
+            <div>
+              <button onClick={calcularSoma}>CALCULAR</button>
+            </div>
+            <div>
+              <p>Resultado: <span>{resultadoSoma}</span></p>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="principal">
-        <div className="cards">
-          <h2>Subtração</h2>
-          <div className="columnLabelWithInput">
-            <label htmlFor="num3">Numero 01</label>
-            <input
-              className="inputs-style"
-              type="number"
-              id="num1"
-              placeholder="Insira um número"
-              value={num3}
-              onChange={(e) => setNum3(e.target.value)}
-            />
-          </div>
-          <div className="columnLabelWithInput">
-            <label htmlFor="num4">Numero 02</label>
-            <input
-              className="inputs-style"
-              type="number"
-              id="num2"
-              placeholder="Insira um número"
-              value={num4}
-              onChange={(e) => setNum4(e.target.value)}
-            />
-          </div>
-          <div>
-            <button onClick={calcularSubtracao}>CALCULAR</button>
-          </div>
-          <div>
-            <p>Resultado: <span>{resultadoSubtracao}</span></p>
+        <div className="principal">
+          <div className="cards">
+            <h2>Subtração</h2>
+            <div className="columnLabelWithInput">
+              <label htmlFor="num3">Numero 01</label>
+              <input
+                className="inputs-style"
+                type="number"
+                id="num1"
+                placeholder="Insira um número"
+                value={num3}
+                onChange={(e) => setNum3(e.target.value)}
+                />
+            </div>
+            <div className="columnLabelWithInput">
+              <label htmlFor="num4">Numero 02</label>
+              <input
+                className="inputs-style"
+                type="number"
+                id="num2"
+                placeholder="Insira um número"
+                value={num4}
+                onChange={(e) => setNum4(e.target.value)}
+                />
+            </div>
+            <div>
+              <button onClick={calcularSubtracao}>CALCULAR</button>
+            </div>
+            <div>
+              <p>Resultado: <span>{resultadoSubtracao}</span></p>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="principal">
-        <div className="cards">
-          <h2>Divisão</h2>
-          <div className="columnLabelWithInput">
-            <label htmlFor="num5">Numero 01</label>
-            <input
-              className="inputs-style"
-              type="number"
-              id="num1"
-              placeholder="Insira um número"
-              value={num5}
-              onChange={(e) => setNum5(e.target.value)}
-            />
-          </div>
-          <div className="columnLabelWithInput">
-            <label htmlFor="num6">Numero 02</label>
-            <input
-              className="inputs-style"
-              type="number"
-              id="num2"
-              placeholder="Insira um número"
-              value={num6}
-              onChange={(e) => setNum6(e.target.value)}
-            />
-          </div>
-          <div>
-            <button onClick={calcularDivisao}>CALCULAR</button>
-          </div>
-          <div>
-            <p>Resultado: <span>{resultadoDivisao}</span></p>
+        <div className="principal">
+          <div className="cards">
+            <h2>Divisão</h2>
+            <div className="columnLabelWithInput">
+              <label htmlFor="num5">Numero 01</label>
+              <input
+                className="inputs-style"
+                type="number"
+                id="num1"
+                placeholder="Insira um número"
+                value={num5}
+                onChange={(e) => setNum5(e.target.value)}
+                />
+            </div>
+            <div className="columnLabelWithInput">
+              <label htmlFor="num6">Numero 02</label>
+              <input
+                className="inputs-style"
+                type="number"
+                id="num2"
+                placeholder="Insira um número"
+                value={num6}
+                onChange={(e) => setNum6(e.target.value)}
+                />
+            </div>
+            <div>
+              <button onClick={calcularDivisao}>CALCULAR</button>
+            </div>
+            <div>
+              <p>Resultado: <span>{resultadoDivisao}</span></p>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="principal">
-        <div className="cards">
-          <h2>Multiplicação</h2>
-          <div className="columnLabelWithInput">
-            <label htmlFor="num7">Numero 01</label>
-            <input
-              className="inputs-style"
-              type="number"
-              id="num1"
-              placeholder="Insira um número"
-              value={num7}
-              onChange={(e) => setNum7(e.target.value)}
-            />
-          </div>
-          <div className="columnLabelWithInput">
-            <label htmlFor="num8">Numero 02</label>
-            <input
-              className="inputs-style"
-              type="number"
-              id="num2"
-              placeholder="Insira um número"
-              value={num8}
-              onChange={(e) => setNum8(e.target.value)}
-            />
-          </div>
-          <div>
-            <button onClick={calcularMultiplicacao}>CALCULAR</button>
-          </div>
-          <div>
-            <p>Resultado: <span>{resultadoMultiplicacao}</span></p>
+        <div className="principal">
+          <div className="cards">
+            <h2>Multiplicação</h2>
+            <div className="columnLabelWithInput">
+              <label htmlFor="num7">Numero 01</label>
+              <input
+                className="inputs-style"
+                type="number"
+                id="num1"
+                placeholder="Insira um número"
+                value={num7}
+                onChange={(e) => setNum7(e.target.value)}
+                />
+            </div>
+            <div className="columnLabelWithInput">
+              <label htmlFor="num8">Numero 02</label>
+              <input
+                className="inputs-style"
+                type="number"
+                id="num2"
+                placeholder="Insira um número"
+                value={num8}
+                onChange={(e) => setNum8(e.target.value)}
+              />
+            </div>
+            <div>
+              <button onClick={calcularMultiplicacao}>CALCULAR</button>
+            </div>
+            <div>
+              <p>Resultado: <span>{resultadoMultiplicacao}</span></p>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="principal">
-        <div className="cards">
-          <h2>Média</h2>
-          <div className="columnLabelWithInput">
-            <label htmlFor="num9">Numero 01</label>
-            <input
-              className="inputs-style"
-              type="number"
-              id="num1"
-              placeholder="Insira um número"
-              value={num9}
-              onChange={(e) => setNum9(e.target.value)}
-            />
-          </div>
-          <div className="columnLabelWithInput">
-            <label htmlFor="num10">Numero 02</label>
-            <input
-              className="inputs-style"
-              type="number"
-              id="num2"
-              placeholder="Insira um número"
-              value={num10}
-              onChange={(e) => setNum10(e.target.value)}
-            />
-          </div>
-          <div className="columnLabelWithInput">
-            <label htmlFor="num11">Numero 03</label>
-            <input
-              className="inputs-style"
-              type="number"
-              id="num2"
-              placeholder="Insira um número"
-              value={num11}
-              onChange={(e) => setNum11(e.target.value)}
-            />
-          </div>
-          <div className="columnLabelWithInput">
-            <label htmlFor="num12">Numero 04</label>
-            <input
-              className="inputs-style"
-              type="number"
-              id="num2"
-              placeholder="Insira um número"
-              value={num12}
-              onChange={(e) => setNum12(e.target.value)}
-            />
-          </div><div className="columnLabelWithInput">
-            <label htmlFor="num13">Numero 05</label>
-            <input
-              className="inputs-style"
-              type="number"
-              id="num2"
-              placeholder="Insira um número"
-              value={num13}
-              onChange={(e) => setNum13(e.target.value)}
-            />
-          </div>
-          <div>
-            <button onClick={calcularMedia}>CALCULAR</button>
-          </div>
-          <div>
-            <p>Resultado: <span>{resultadoMedia}</span></p>
+        <div className="principal">
+          <div className="cards">
+            <h2>Média</h2>
+            <div className="columnLabelWithInput">
+              <label htmlFor="num9">Numero 01</label>
+              <input
+                className="inputs-style"
+                type="number"
+                id="num1"
+                placeholder="Insira um número"
+                value={num9}
+                onChange={(e) => setNum9(e.target.value)}
+                />
+            </div>
+            <div className="columnLabelWithInput">
+              <label htmlFor="num10">Numero 02</label>
+              <input
+                className="inputs-style"
+                type="number"
+                id="num2"
+                placeholder="Insira um número"
+                value={num10}
+                onChange={(e) => setNum10(e.target.value)}
+                />
+            </div>
+            <div className="columnLabelWithInput">
+              <label htmlFor="num11">Numero 03</label>
+              <input
+                className="inputs-style"
+                type="number"
+                id="num2"
+                placeholder="Insira um número"
+                value={num11}
+                onChange={(e) => setNum11(e.target.value)}
+                />
+            </div>
+            <div className="columnLabelWithInput">
+              <label htmlFor="num12">Numero 04</label>
+              <input
+                className="inputs-style"
+                type="number"
+                id="num2"
+                placeholder="Insira um número"
+                value={num12}
+                onChange={(e) => setNum12(e.target.value)}
+                />
+            </div><div className="columnLabelWithInput">
+              <label htmlFor="num13">Numero 05</label>
+              <input
+                className="inputs-style"
+                type="number"
+                id="num2"
+                placeholder="Insira um número"
+                value={num13}
+                onChange={(e) => setNum13(e.target.value)}
+                />
+            </div>
+            <div>
+              <button onClick={calcularMedia}>CALCULAR</button>
+            </div>
+            <div>
+              <p>Resultado: <span>{resultadoMedia}</span></p>
+            </div>
           </div>
         </div>
       </div>
--- 4Abas/src/Mapa.jsx
+++ 4Abas/src/Mapa.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
 import style from './Mapa.module.css'
+import Menu from './components/Menu';
+
+import { MapContainer, TileLayer, Marker, Popup} from 'react-leaflet'
 
 export default function Mapa() {
-  return (
-    <div className={style.firstDiv}>
+  const position = [51.505, -0.09]
+  return(
+    <div>
+      <Menu/>
       <h1>Mapa</h1>
+      <br />
+      <div>
+        <div className={style.wrapMap}>
+          <h2>Map</h2>
+          <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+            <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={position}>
+              <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+              </Popup>
+            </Marker>
+          </MapContainer>,
+        </div>
+      </div>
     </div>
   );
 }
